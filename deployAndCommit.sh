f894273471c98d11c97542dc6576bc6d3da16681
Julindada <julin1184012246@gmail.com>
2019-01-08 18:01:58 +0800

    deploy and commit all 2019-01-08 18:01:58

--- deployAndCommit.sh
+++ deployAndCommit.sh
@@ -1,9 +1,11 @@
 #! /bin/bash
 hexo clean
 hexo g -d
-git add .deploy_git/ db.json public/
-var1=date +%Y-%m-%d
+git add _config.yml        deployAndCommit.sh package-lock.json  public             source
+git add db.json   .deploy_git    node_modules       package.json       scaffolds          themes
+var1=$(date "+%Y-%m-%d %H:%M:%S")
 var2="deploy and commit all "$var1
-git commit -a -m $var2
+echo $var2
+git commit -a -m "$var2"
 git push origin blogsrc
 git status
